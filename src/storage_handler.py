1442b900949965568d245bcc5932559bf030d55b
Mateusz Jaworski <jaworskimat.priv@gmail.com>
2024-12-18 19:35:54 +0100

    writing data to csv files

--- src/storage_handler.py
+++ src/storage_handler.py
@@ -1,10 +1,19 @@
 import csv
 
 # read csv data from path
-def read_from_file(path: str = "data/students.csv"):
-    data = []
+def read_from_file(path: str):
     try:
         with open(path, 'r') as file:
             return list(csv.reader(file))
     except FileNotFoundError:
         return None
+    
+# write 
+def write_row_to_file(path: str, data):
+    with open(path, 'w') as file:
+            csv.writer(file).writerow(data)
+
+# write 
+def write_rows_to_file(path: str, data):
+    with open(path, 'w') as file:
+            csv.writer(file).writerows(data)
